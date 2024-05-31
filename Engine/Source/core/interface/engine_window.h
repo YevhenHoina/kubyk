8bd2e29a00fec95e7dd276c96dc32bc959512f64
YevhenHoina <yevhenhoina@gmail.com>
2024-05-31 15:05:13 +0300

    moved all core files from includes away

---
+++ Engine/Source/core/interface/engine_window.h
@@ -0,0 +1,2 @@
+#include "core/gameWindow/game.h"
+
---
+++ Engine/Source/core/interface/scene.h
@@ -0,0 +1,21 @@
+#pragma once
+
+void inspector()
+{
+
+}
+
+void details()
+{
+
+}
+
+void scene()
+{
+#include "gameWindow/game.h"
+}
+
+void preview()
+{
+
+}
