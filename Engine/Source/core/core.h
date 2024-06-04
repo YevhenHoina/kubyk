798189383d1502c9d4b3a5d4b503d18d04fa4516
YevhenHoina <yevhenhoina@gmail.com>
2024-06-04 08:29:11 +0300

    update dependencies

---
+++ Engine/Source/core/core.h
@@ -0,0 +1,56 @@
+#pragma once
+
+//std includes
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <memory>
+
+//imgui includes
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+//OpenGL inludes
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLSW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+
+
+
+
+
+struct COLOR {
+    GLclampf R;
+    GLclampf G;
+    GLclampf B;
+    GLclampf A;
+};
+
+struct kb_Vector
+{
+    float X;
+    float Y;
+    float Z;
+};
+
+#include "core/gameWindow/Shader.h"
+const glm::vec3 vec3ZERO = glm::vec3(0.0f, 0.0f, 0.0f);
+
+
+
